3a8cac089c3f1a070bf6fdc49186ea8736bdd33d
Yasin Hatibi <yhatibi@gmail.com>
2021-04-12 03:12:37 +0200

    Ui update

---
+++ lib/Screens/Chat/screens/messages/components/body.dart
@@ -0,0 +1,27 @@
+import 'package:blue_car/constants.dart';
+import 'package:blue_car/Screens/Chat/models/ChatMessage.dart';
+import 'package:flutter/material.dart';
+
+import 'chat_input_field.dart';
+import 'message.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+            child: ListView.builder(
+              itemCount: demeChatMessages.length,
+              itemBuilder: (context, index) =>
+                  Message(message: demeChatMessages[index]),
+            ),
+          ),
+        ),
+        ChatInputField(),
+      ],
+    );
+  }
+}
